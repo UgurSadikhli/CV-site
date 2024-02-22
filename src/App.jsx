1a4f6b44b0482481641f6a69315e2e21ab70ee30
Vuqar <69174569+vugito@users.noreply.github.com>
2024-02-22 14:56:43 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,16 +29,21 @@ function App() {
           <Route index element={<Login />} />
           <Route path="login" element={<Login />} />
           <Route path="logup" element={<Logup />} />
+          <Route path="*" element={
+            <div>Page not found</div>
+          } />
+
           <Route
             path="CV"
             element={
               isAuthenticated ? (
-                <CV onLogout={handleLogout} />
+                <CV/>
               ) : (
                 <Navigate to="/" />
               )
             }
           />
+
         </Route>
       </Routes>
     </Router>
